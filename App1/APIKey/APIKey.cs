7410e97ed985e7e593008c28f6aff1149f9bc1a3
Fumiya Kume <fumiya.kume@hotmail.com>
2018-03-24 11:14:30 +0900

    API Key を保管するためのクラスを追加

---
+++ App1/APIKey/APIKey.cs
@@ -0,0 +1,10 @@
+﻿namespace App1.APIKey
+{
+    public class APIKey
+    {
+        public string FaceApiKey { get; set; } = "You hsould Replace Face API Key";
+        public string FaceApiPath { get; set; } = "You Should Replace FaceAPI Path";
+        public string CustomVisonKey { get; set; } = "You Should Replace Custmom Vison Key";
+        public string CustomVisonPath { get; set; } = "You Should Replace Cusotm Vison Path";
+    }
+}
